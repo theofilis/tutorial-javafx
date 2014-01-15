e2d46f0fb902c1cfd62a17125170dfdeef4b4d2c
theofilis <theofilis.g@gmail.com>
2014-01-16 00:27:25 +0200

    Add FXML Form Example

---
+++ FXML Form Example/gr/theofilis/Controller.java
@@ -0,0 +1,19 @@
+package gr.theofilis;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private GridPane pane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
